fa56c309b661acbea7e4f50eb535f65669417ce5
Yuriy <yuriypilot83@gmail.com>
2020-11-15 22:21:46 +0300

    Ugadajka

---
+++ app/src/main/java/com/example/ugadaikakun/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.ugadaikakun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvInfo;
+    EditText etInput;
+    Button bControl;
+    int guess;
+    boolean gameFinished;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvInfo = (TextView)findViewById(R.id.textView);
+        etInput = (EditText)findViewById(R.id.Number);
+        bControl = (Button)findViewById(R.id.button);
+
+        guess = (int)(Math.random()*100);
+        gameFinished = false;
+
+    }
+
+    public void onClick(View v){
+
+        if (!gameFinished) {
+            int inp = Integer.parseInt(etInput.getText().toString());
+            if (inp > guess)
+                tvInfo.setText(getResources().getString(R.string.ahead));
+            if (inp < guess)
+                tvInfo.setText(getResources().getString(R.string.behind));
+            if (inp == guess){
+                tvInfo.setText(getResources().getString(R.string.hit));
+                bControl.setText(getResources().getString(R.string.play_more));
+                gameFinished = true;
+            }
+        }
+        else {
+            guess = (int)(Math.random()*100);
+            bControl.setText(getResources().getString(R.string.input_value));
+            tvInfo.setText(getResources().getString(R.string.try_to_guess));
+            gameFinished = false;
+        }
+    }
+
+}
